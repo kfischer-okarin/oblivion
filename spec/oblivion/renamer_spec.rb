96136dc8c7b866c050ae72202ba7cfba46b63282
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-08-24 13:19:43 +0900

    Reorganize and add more renamer spec

--- spec/oblivion/renamer_spec.rb
+++ spec/oblivion/renamer_spec.rb
@@ -2,21 +2,45 @@
 
 require_relative '../spec_helper'
 
-RSpec.describe Oblivion::Renamer::Random do
+RSpec.describe Oblivion::Renamer do
   let(:renamer) { described_class.new }
 
-  describe '#rename' do
-    subject(:rename) { renamer.rename(old_name) }
-    let(:old_name) { :method_name }
+  shared_examples 'Renamer' do
+    describe 'Renamer common features' do
+      let(:old_name) { 'old_name' }
 
-    it 'creates a 11 character random name' do
-      rename
+      describe '#rename' do
+        it 'marks the name as renamed' do
+          expect { renamer.rename(old_name) }.to(change { renamer.was_renamed?(old_name) }.from(false).to(true))
+        end
 
-      expect(renamer.new_name_of(old_name)).to be_a(String).and have_attributes(size: 11)
+        it 'creates a new name' do
+          renamer.rename(old_name)
+          expect(renamer.new_name_of(old_name)).not_to eq old_name
+        end
+
+        it 'creates a different name for each method' do
+          another_name = 'another_name'
+          renamer.rename(old_name)
+          renamer.rename(another_name)
+          expect(renamer.new_name_of(old_name)).not_to eq renamer.new_name_of(another_name)
+        end
+      end
     end
+  end
 
-    it 'marks the name as renamed' do
-      expect { rename }.to(change { renamer.was_renamed?(old_name) }.from(false).to(true))
+  describe Oblivion::Renamer::Random do
+    describe '#rename' do
+      subject(:rename) { renamer.rename(old_name) }
+      let(:old_name) { :method_name }
+
+      include_examples 'Renamer'
+
+      it 'creates a 11 character random name' do
+        rename
+
+        expect(renamer.new_name_of(old_name)).to be_a(String).and have_attributes(size: 11)
+      end
     end
   end
 end

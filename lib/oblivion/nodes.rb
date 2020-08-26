3f95f7c6e859bb478ddcacef8ccdb78623ad73e2
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-08-26 13:07:55 +0900

    Add some comments and suppress smell for node classess

--- lib/oblivion/nodes.rb
+++ lib/oblivion/nodes.rb
@@ -22,19 +22,24 @@ module Oblivion
       const_defined?(name, false)
     end
 
+    # :reek:IrresponsibleModule
     class Base < Parser::AST::Node
       class << self
+        # Class macro for defining accessors for node children
         def children(*attribute_names)
           attribute_names.each.with_index do |name, index|
             child(name, index)
           end
         end
 
+        # Class macro for defining accessors for a node child at a particular index
+        # Pass a range as index to get an accessor for a range of children
         def child(name, index)
           define_getter(name, index)
           define_update_method(name, index)
         end
 
+        # Class macro for defining `renamed` methods on node classes
         def renameable(attribute = :name)
           define_method :renamed do |renamer|
             send :"with_#{attribute}", renamer.new_name_of(send(attribute))
@@ -67,17 +72,21 @@ module Oblivion
       end
     end
 
+    # :reek:IrresponsibleModule
     class Class < Base
     end
 
+    # :reek:IrresponsibleModule
     class Sclass < Class; end
 
+    # :reek:IrresponsibleModule
     class Def < Base
       children :name, :args, :body
 
       renameable
     end
 
+    # :reek:IrresponsibleModule
     class Send < Base
       children :receiver, :method_name
 
@@ -90,6 +99,7 @@ module Oblivion
       end
     end
 
+    # :reek:IrresponsibleModule
     class Ivar < Base
       renameable
 
@@ -108,20 +118,25 @@ module Oblivion
       end
     end
 
+    # :reek:IrresponsibleModule
     class Ivasgn < Ivar
       child :value, 1
     end
 
+    # :reek:IrresponsibleModule
     class Sym < Base
       children :name
 
       renameable
     end
 
+    # :reek:IrresponsibleModule
     class Arg < Sym; end
 
+    # :reek:IrresponsibleModule
     class Lvar < Arg; end
 
+    # :reek:IrresponsibleModule
     class Lvasgn < Lvar
       child :value, 1
     end

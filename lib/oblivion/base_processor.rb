36fc9a09e5762501d40f8fda6030f47b0563f540
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-08-27 13:55:20 +0900

    Add comment to base processor

--- lib/oblivion/base_processor.rb
+++ lib/oblivion/base_processor.rb
@@ -4,6 +4,8 @@ require 'ast'
 require 'parser'
 
 module Oblivion
+  # Parent class of all processors in this gem
+  # Makes sure that nodes are wrapped into the right subclass
   class BaseProcessor < Parser::AST::Processor
     def process(node)
       return node unless node.is_a? AST::Node

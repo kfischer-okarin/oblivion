8c5fbb90c8b3ac0a932d6812024b3a3ea93cd3a4
Kevin Fischer <kevin@agileware.jp>
2020-09-04 15:50:47 +0900

    Update oblivion_spec.rb

--- spec/oblivion_spec.rb
+++ spec/oblivion_spec.rb
@@ -14,16 +14,6 @@ RSpec.describe Oblivion do
       RUBY
     }
 
-    it 'parses the source uglifies it and produces resulting source code' do
-      expect(Unparser).to receive(:parse).with(source).and_return :parsed_ast
-      expect(Oblivion::RewriteMethodContainer).to receive(:process).with(:parsed_ast, Oblivion::Renamer::Random)
-                                                                   .and_return :uglified_ast
-      expect(Unparser).to receive(:unparse).with(:uglified_ast).and_return 'uglified_source'
-      expect(result).to eq 'uglified_source'
-    end
-
-    it 'does not produce new lines' do
-      expect(result).not_to include("\n")
-    end
+    
   end
 end
